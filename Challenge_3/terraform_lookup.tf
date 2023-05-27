72395297fe356481a772ad4114ff22fe5c7a5e6b
Bharani NVN <bharini.naga@matrixcare.com>
2023-05-27 19:56:31 +0530

    Add Challenge 3 files 0.2

--- Challenge_3/terraform_lookup.tf
+++ Challenge_3/terraform_lookup.tf
@@ -1,15 +1,15 @@
 variable "my_object" {
   type = map(string)
   default = {
-    key1 = "value1"
-    key2 = "value2"
-    key3 = "value3"
+    key1 = "Azure"
+    key2 = "Aws"
+    key3 = "Gcp"
   }
 }
 
 locals {
   get_value =  # Function to get value by key from object
-    lambda(object, key) -> string {
+    cloud(object, key) -> string {
       return element(object, key)
     }
 }
